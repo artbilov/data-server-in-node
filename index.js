55cca14cf30375a4360c1cf1e9913d94d866e4d0
artbilov <artbilov@gmail.com>
2024-05-08 11:52:48 -0700

    add port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require('http').createServer();
 
-server.listen(3000, () => console.log('http://localhost:3000'));
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log('http://localhost:' + port));
 server.on('request', handleRequest);
 
 function handleRequest(request, response) {
