21c47642880c6dcab8bc89e8adc1c55753c56404
artbilov <artbilov@gmail.com>
2024-05-08 11:48:54 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const server = require('http').createServer();
+
+server.listen(3000, () => console.log('http://localhost:3000'));
+server.on('request', handleRequest);
+
+function handleRequest(request, response) {
+  if (request.method === 'GET') return response.end(page);
+
+  let body = '';
+
+  request.on('data', chunk => body += chunk);
+  request.on('end', () => {
+    try {
+      body = JSON.parse(body);
+    } catch { }
+
+    const entries = Object.entries(body);
+    const json = JSON.stringify(entries);
+    
+    console.log('received:', body);
+    console.log('sent:', entries);
+    response.writeHead(200, {
+      'Content-Type': 'text/json',
+      'Access-Control-Allow-Origin': '*',
+    }).end(json);
+  })
+}
+
+const page = `<script>
+  const body = JSON.stringify({first: 'John', last: 'Doe', age: 33});
+
+  setTimeout(() => document.body.innerText += 'sent: ' + body + '\\n');
+  
+  fetch('/', {method: 'POST', body})
+    .then(response => response.json())
+    .then(data => document.body.append('received: ' + JSON.stringify(data)));
+</script>`;
